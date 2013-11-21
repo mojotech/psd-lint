541be188f3c6d14bf1de108c2fe1f617062e6d4c
Sam Saccone <samccone@gmail.com>
2013-11-21 10:58:54 -0500

    respond with the parsed PSD as json

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
+require 'json'
 require 'sinatra'
 require 'haml'
 require 'psd'
 
 get '/', :provides => 'html' do
   haml :index
+end
+
+post '/', :provides => 'json' do
+  PSD.open(params['psd'][:tempfile]) do |psd|
+    return psd.tree.to_hash.to_json
+  end
 end
