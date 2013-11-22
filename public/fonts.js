dc92d0fdfe71d48de42e517851171902243d3c8f
Sam Saccone <samccone@gmail.com>
2013-11-22 09:45:37 -0500

    enable functional property lookups

--- public/fonts.js
+++ public/fonts.js
@@ -3,43 +3,21 @@
 
   LNT.getFontNames = function() {
     return _(PSD.children).map(function(v) {
-      return getNodeFontNames(v);
+      return LNT.propertyWalker(v, function(node, p) {
+        if (node.text) {
+          p.push(node.text.font.name);
+        }
+      });
     }).flatten().uniq().valueOf();
-  }
+  };
 
   LNT.getFontSizes = function() {
     return _(PSD.children).map(function(v) {
-      return getNodeFontSizes(v);
+      return LNT.propertyWalker(v, function(node, p) {
+        if (node.text) {
+          p.push(node.text.font.sizes[0]);
+        }
+      });
     }).flatten().valueOf();
-  }
-
-  function getNodeFontSizes(node) {
-    var sizes = [];
-    if (node.text) {
-      sizes.push(node.text.font.sizes[0]);
-    }
-
-    if (node.children) {
-      _.flatten(_.each(node.children, function(n) {
-        sizes.push(getNodeFontSizes(n));
-      }));
-    }
-
-    return sizes;
-  }
-
-  function getNodeFontNames(node) {
-    var fonts = [];
-    if (node.text) {
-      fonts.push(node.text.font.name);
-    }
-
-    if (node.children) {
-      _.flatten(_.each(node.children, function(n) {
-        fonts.push(getNodeFontNames(n));
-      }));
-    }
-
-    return fonts;
-  }
+  };
 }());
--- public/walker.js
+++ public/walker.js
@@ -2,7 +2,13 @@
   LNT = window.LNT || {};
 
   LNT.propertyWalker = function(node, prop) {
-    var properties = [node[prop]];
+    var properties = [];
+
+    if (_.isFunction(prop)) {
+      prop(node, properties);
+    } else {
+      properties.push(node[prop]);
+    }
 
     if (node.children) {
       _.flatten(_.each(node.children, function(n) {
