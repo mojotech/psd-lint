a16f0b1b39f5015ce2ff8003449ee11907a85b93
Sam Saccone <samccone@gmail.com>
2013-11-22 11:18:44 -0500

    rework font name processer to add layer names

--- public/fonts.js
+++ public/fonts.js
@@ -2,13 +2,22 @@
   LNT = window.LNT || {};
 
   LNT.getFontNames = function() {
-    return _(PSD.children).map(function(v) {
-      return LNT.propertyWalker(v, function(node, p) {
+    var fonts = {};
+    _(PSD.children).each(function(v) {
+      LNT.propertyWalker(v, function(node, p) {
         if (node.text) {
-          p.push(node.text.font.name);
+          if (typeof fonts[node.text.font.name] === "undefined") {
+            fonts[node.text.font.name] = {
+              layers: [node.name]
+            }
+          } else {
+            fonts[node.text.font.name].layers.push(node.name);
+          }
         }
       });
-    }).flatten().uniq().valueOf();
+    });
+
+    return fonts;
   };
 
   LNT.getFontSizes = function() {
