d4321e38c9803a7c79c1ac00e8c5f8c2cc3c4fce
Sam Saccone <samccone@gmail.com>
2013-11-25 16:35:51 -0500

    convert server to only act as a worker

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 ruby '1.9.3'
 
-gem 'haml', '4.0.4'
 gem 'psd_native', '0.5.0'
 gem 'oj', '2.2.3'
 gem 'sinatra', '1.4.4'
--- app.rb
+++ app.rb
@@ -1,19 +1,11 @@
 require 'oj'
 require 'sinatra'
-require 'haml'
 require 'psd_native'
 
-get '/', :provides => 'html' do
-  haml :index
-end
+post '/', :provides => 'json' do
+  content_type :json
 
-get '/grade', :provides => 'html' do
-  haml :grade
-end
-
-post '/', :provides => 'html' do
   PSD.open(params['psd'][:tempfile]) do |psd|
-    @tree = psd.tree.to_hash
-    haml :grade
+    Oj.dump(psd.tree.to_hash, mode: :compat)
   end
 end
