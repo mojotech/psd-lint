e5610287e49b91ac0f14c1d04c68073856ad0d3c
Sam Saccone <samccone@gmail.com>
2013-12-02 11:14:18 -0500

    enable cross origin requests for uploads

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'oj'
 require 'sinatra'
 require 'psd_native'
+require 'sinatra/cross_origin'
+
+configure do
+  enable :cross_origin
+end
+
+set :allow_origin, :any
+set :allow_methods, [:get, :post, :options]
+set :allow_credentials, true
+set :max_age, "1728000"
+set :expose_headers, ['Content-Type']
+set :protection, :except => [:http_origin]
+
+options '/' do
+  200
+end
 
 post '/', :provides => 'json' do
   content_type :json
